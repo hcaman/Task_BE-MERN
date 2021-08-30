7e008045d5f5c48f657a0da07eefcd96fe0580e1
hugo cm <hugocmdesign@gmail.com>
2021-08-30 16:57:20 +0200

    add check and show error express validator

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,6 +1,13 @@
 const Proyecto = require('../models/Proyecto');
+const { validationResult } = require('express-validator');
 
 exports.crearProyecto = async (req, res) => {
+  const errores = validationResult(req);
+
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ Errors: errores.array() });
+  }
+
   try {
     const proyecto = new Proyecto(req.body);
 
--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const { check } = require('express-validator');
 const router = express.Router();
 const proyectoController = require('../controllers/proyectoController');
 const auth = require('../middelware/auth');
 
-router.post('/', auth, proyectoController.crearProyecto);
+router.post(
+  '/',
+  auth,
+  [check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()],
+  proyectoController.crearProyecto
+);
 router.get('/', auth, proyectoController.crearProyecto);
 
 module.exports = router;
