cb27bf0ddaddbc74af35537919c48595052799ee
hugo cm <hugocmdesign@gmail.com>
2021-08-30 14:44:48 +0200

    fn for save the user in DB

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,14 @@
-exports.crearUsuario = (req, res) => {
+const Usuario = require('../models/Usuario');
+exports.crearUsuario = async (req, res) => {
   // console.log(req.body);
+  try {
+    let usuario;
+    usuario = new Usuario(req.body);
+    console.log(usuario);
+    await usuario.save();
+    res.send('Usuario creado correctamente');
+  } catch (error) {
+    console.log('Error:' + error);
+    res.status(400).send('Hubo un error');
+  }
 };
