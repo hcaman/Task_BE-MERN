3de907643a8b97f296a1a71b7af6a3b0261aa9f6
hugo cm <hugocmdesign@gmail.com>
2021-09-04 00:12:14 +0200

    Fix some bugs

--- config/db.js
+++ config/db.js
@@ -6,8 +6,6 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_MONGO, {
       useNewUrlParser: true,
-      // useUnifiedTopology: true,
-      // useFindAndModify: false,
     });
     console.log('DB Connected');
   } catch (error) {
--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(cors());
 
 app.use(express.json({ extended: true }));
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
-app.listen(PORT, () => {
-  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
